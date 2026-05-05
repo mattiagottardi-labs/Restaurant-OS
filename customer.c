039ff17bf9d3c4833d41ed6f2f7540f39c9a1ab0
mattiagottardi-labs <mattia.gottardi-1@studenti.unitn.it>
2026-05-05 16:48:50 +0200

    made order struct and make_order function to generate random orders

--- customer.c
+++ customer.c
@@ -3,13 +3,19 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-int main(){
-    char* file_location = "C:\\Users\\artin\\Restaurant-OS\\resources\\2026-project-5\\code\\menu.csv";
-    printf("opening file...\n");
-    make_menu(file_location, &Menu, 20);
-    printf("menu made, num_dishes: %d\n", Menu.num_dishes);
-    for(int i = 0; i < Menu.num_dishes; i++){
-        printf("dish %d: %s\n", i, Menu.selection[i]->name);
+
+typedef struct order{
+    dish** dishes;
+    int patience;
+}order;
+
+order* make_order(int num_dishes){
+    srand(time(NULL));
+    order *o;
+    o->dishes = malloc(num_dishes*sizeof(dish));
+    for(int i = 0; i < num_dishes; i++){
+        o->dishes[i] = Menu.selection[rand()%20];
     }
-    return 0;
+    o->patience = rand()%100;
+    return o;
 }
